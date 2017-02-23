b685ceb343a2837194326b003629fe651f7d4967
Vladimir Litovkin <vladimir.litovkin@e-solutions.cz>
2017-02-23 07:39:57 +0100

    Drawable back compatibility

--- app/src/main/java/com/incrazing/harrycalendar/app/Config.java
+++ app/src/main/java/com/incrazing/harrycalendar/app/Config.java
@@ -1,6 +1,7 @@
 package com.incrazing.harrycalendar.app;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 
@@ -24,6 +25,21 @@ public class Config {
                 return context.getResources().getColor(id);
             } catch (Exception e) {
                 return context.getResources().getColor(R.color.RED_200);
+            }
+        }
+    }
+    public static Drawable getDrawable(int id, Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                return ContextCompat.getDrawable(context, id);
+            } catch (Exception e) {
+                return ContextCompat.getDrawable(context, R.color.RED_200);
+            }
+        } else {
+            try {
+                return context.getResources().getDrawable(id);
+            } catch (Exception e) {
+                return context.getResources().getDrawable(R.color.RED_200);
             }
         }
     }
